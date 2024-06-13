721e318e84fc36663652f92d8cdf1634772bae12
ShinDongju <dongju010@kakao.com>
2024-06-13 23:43:36 +0900

    UPDATE: 알람히스토리 기반 다지기

--- src/app/alarm-history/alarm-history.controller.ts
+++ src/app/alarm-history/alarm-history.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AlarmHistoryService } from './alarm-history.service';
+import { AccessTokenGuard } from 'src/auth/guards/access-token.guard';
 
 @Controller('alarm-history')
-export class AlarmHistoryController {}
+export class AlarmHistoryController {
+  constructor(private readonly alarmHistoryService: AlarmHistoryService) {}
+
+  @UseGuards(AccessTokenGuard)
+  @Get('/')
+  async getAlarmHistoryList(@Req() req: Request) {
+    return await this.alarmHistoryService.getAlarmHistoryList();
+  }
+}
--- src/app/alarm-history/alarm-history.service.ts
+++ src/app/alarm-history/alarm-history.service.ts
@@ -9,6 +9,8 @@ export class AlarmHistoryService {
   constructor(
     @InjectRepository(AlarmHistory)
     private readonly alarmHistoryRepository: Repository<AlarmHistory>,
+    @InjectRepository(AlarmHistory)
+    private albumBoardRepository: Repository<AlbumBoard>,
   ) {}
 
   /** 사용자의 활동에 대한 내역 저장 */
@@ -36,6 +38,33 @@ export class AlarmHistoryService {
     } catch (error) {
       Logger.error(error);
       throw new HttpException('저장에 실패했습니다.', 500);
+    }
+  }
+
+  async getAlarmHistoryList(req: any) {
+    const { coupleId } = req.user;
+    try {
+      const alarmHistoryList = await this.alarmHistoryRepository.find({
+        where: { coupleId: coupleId },
+        order: { createdAt: 'DESC' },
+      });
+
+      const testList = await this.alarmHistoryList
+        .createQueryBuilder('post')
+        .leftJoinAndSelect('post.files', 'file')
+        .select([
+          'post.oid',
+          'post.title',
+          // Add other post columns you want to select
+          `json_agg(file.filename) AS filelist`,
+        ])
+        .groupBy('post.oid')
+        .getRawMany();
+
+      return alarmHistoryList;
+    } catch (error) {
+      Logger.error(error);
+      throw new HttpException('알람 히스토리 조회에 실패했습니다.', 500);
     }
   }
 }
